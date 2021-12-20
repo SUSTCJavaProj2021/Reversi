c5449064cbe4a3fb369ddb400c0765b6373952ef
IskXCr <IshikiEnhdX@outlook.com>
2021-12-20 18:20:08 +0800

    Update design

--- src/main/java/com/demo/reversi/component/gamemodel/BoardGridComponent.java
+++ src/main/java/com/demo/reversi/component/gamemodel/BoardGridComponent.java
@@ -4,6 +4,7 @@ import com.demo.reversi.logger.Log0j;
 import com.demo.reversi.themes.Theme;
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
